842e2c6d014b5f53accb12b90982f2373ca71193
tada <tada3@outlook.com>
2019-04-21 12:13:48 +0900

    more readable.

---
+++ README.md
@@ -0,0 +1,5 @@
+# AND Operator
+
+- 学習なし。
+- 正しいパラメータを与えて動作を見る。
+- 参考: https://nixeneko.hatenablog.com/entry/2017/06/01/021626
--- and_op.py
+++ and_op.py
@@ -1,19 +1,21 @@
-
 import numpy as np
 import chainer.links as L
 import chainer.functions as F
 
 print("START!")
 
-W = np.array([[1, 1]])
-b = -1.0 # bias
-p = L.Linear(2, 1, initialW=W, initial_bias=b)
-f = F.relu
+weight = np.array([[1, 1]])
+bias = -1.0
+layer1 = L.Linear(2, 1, initialW=weight, initial_bias=bias)
+af1 = F.relu
 
 def AND(x):
-    return f(p(x))
+    return af1(layer1(x))
 
-x = np.array([[1,1],[0,1],[1,0],[0,0]], dtype=np.float32) # input data
-print(AND(x).data)
+in_data = np.array([[1,1],[0,1],[1,0],[0,0]], dtype=np.float32) # input data
+out_data = AND(in_data).data
+
+for i in range(0, len(in_data)):
+    print(in_data[i], '->', out_data[i])
 
 print('DONE!')
